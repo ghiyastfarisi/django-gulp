703e65ec7a96468c798a360566d2d43e8f62340b
Stuart MacKay <smackay@flagstonesoftware.com>
2017-12-12 20:41:16 +0000

    Finished the minimum viable version of the gulp config.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,21 +1,36 @@
 var gulp = require('gulp');
 var del = require('del');
 
+var images = ['images/**/*.jpg', 'images/**/*.gif', 'images/**/*.svg'];
+var scripts = ['scripts/**/*.js'];
+var styles = ['styles/**/*.css'];
+
 
 gulp.task('clean', del.bind(null, ['dist']));
 
-gulp.task('build', ['styles', 'scripts']);
+gulp.task('build', ['images', 'scripts', 'styles']);
 
 gulp.task('default', ['clean'], function() {
   gulp.start('build')
 });
 
-gulp.task('styles', function() {
-  return gulp.src('styles/**/*.css')
-    .pipe(gulp.dest('dist/css'))
+gulp.task('images', function() {
+  return gulp.src(images)
+    .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('scripts/**/*.js')
+  return gulp.src(scripts)
     .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('styles', function() {
+  return gulp.src(styles)
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(images, ['images'])
+  gulp.watch(scripts, ['scripts'])
+  gulp.watch(styles, ['styles'])
 });
